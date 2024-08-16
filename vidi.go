5d84ab716fc994353f8a1694a9b6226be5bf1f29
Matthew Salvatore Giancola <matthew.giancola42@gmail.com>
2024-08-16 15:12:56 -0400

    updated components of vidi

--- vidi.go
+++ vidi.go
@@ -1,28 +1,24 @@
-package vidi 
+package vidi
 
-import(
-	"context"
+import (
 	"database/sql"
-	"encoding/json"
 	"fmt"
-	"io"
-	"log"
 	"net/http"
-	"time"
 )
 
-/**
+/*
+*
 Record
 Head: the main source (key) of this data
 Body: the main content (value) of this data
-Tail: links to forms specific and general 
+Tail: links to forms specific and general
 Sign: Signature of the member who created the record
 Mark: Datetime stamp with location where the source came from
 Code: Request URL given and body
 Line: Unique auto generated identifier for this line
 */
 type Record struct {
-	Head string 
+	Head string
 	Body string
 	Tail string
 	Code string
@@ -31,9 +27,10 @@ type Record struct {
 	Line uint64
 }
 
-/**
+/*
+*
 Access
-Record: The record to which to grant access 
+Record: The record to which to grant access
 Member: The member to whom to grant access
 Number: Targeting record number value
 */
@@ -43,9 +40,10 @@ type Access struct {
 	Number uint64
 }
 
-/**
+/*
+*
 Member
-Name: 
+Name:
 */
 type Member struct {
 	Name string
@@ -53,67 +51,65 @@ type Member struct {
 	Peer uint64
 }
 
-
-
 type Service struct {
-	db *sql.DB // Database connection pool.
-
+	db  *sql.DB // Database connection pool.
+	err error
 }
 
-func (s *Service) queryAllByHead(name string)([]Record, error){
+func (s *Service) queryAllByHead(name string) ([]Record, error) {
 	// An records slice to hold data from returned rows.
-    var records []Record
-    rows, err := db.Query("SELECT * FROM record WHERE recordhead = ?", name)
-    if err != nil {
-        return nil, fmt.Errorf("search %q: %v", name, err)
-    }
-    defer rows.Close()
-    // Loop through rows, using Scan to assign column data to struct fields.
-    for rows.Next() {
-        var alb Record
-        if err := rows.Scan(&alb.Line, &alb.Sign, &alb.Code, &alb.Mark, &alb.Head, &alb.Body, &alb.Tail); err != nil {
-            return nil, fmt.Errorf("search %q: %v", name, err)
-        }
-        records = append(records, alb)
-    }
-    if err := rows.Err(); err != nil {
-        return nil, fmt.Errorf("search %q: %v", name, err)
-    }
-    return records, nil
+	var records []Record
+	rows, err := s.db.Query("SELECT * FROM record WHERE recordhead = ?", name)
+	if err != nil {
+		return nil, fmt.Errorf("search %q: %v", name, err)
+	}
+	defer rows.Close()
+	// Loop through rows, using Scan to assign column data to struct fields.
+	for rows.Next() {
+		var alb Record
+		if err := rows.Scan(&alb.Line, &alb.Sign, &alb.Code, &alb.Mark, &alb.Head, &alb.Body, &alb.Tail); err != nil {
+			return nil, fmt.Errorf("search %q: %v", name, err)
+		}
+		records = append(records, alb)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, fmt.Errorf("search %q: %v", name, err)
+	}
+	return records, nil
 }
 
-func (s *Service) query(name string, )([]Record, error){
+func (s *Service) query(name string) ([]Record, error) {
 	// An records slice to hold data from returned rows.
-    var records []Record
-    rows, err := db.Query("SELECT * FROM record WHERE head = ?", name)
-    if err != nil {
-        return nil, fmt.Errorf("search %q: %v", name, err)
-    }
-    defer rows.Close()
-    // Loop through rows, using Scan to assign column data to struct fields.
-    for rows.Next() {
-        var alb Record
-        if err := rows.Scan(&alb.Line, &alb.Sign, &alb.Code, &alb.Mark, &alb.Head, &alb.Body, &alb.Tail); err != nil {
-            return nil, fmt.Errorf("search %q: %v", name, err)
-        }
-        records = append(records, alb)
-    }
-    if err := rows.Err(); err != nil {
-        return nil, fmt.Errorf("search %q: %v", name, err)
-    }
-    return records, nil
+	var records []Record
+	rows, err := s.db.Query("SELECT * FROM record WHERE head = ?", name)
+	if err != nil {
+		return nil, fmt.Errorf("search %q: %v", name, err)
+	}
+	defer rows.Close()
+	// Loop through rows, using Scan to assign column data to struct fields.
+	for rows.Next() {
+		var alb Record
+		if err := rows.Scan(&alb.Line, &alb.Sign, &alb.Code, &alb.Mark, &alb.Head, &alb.Body, &alb.Tail); err != nil {
+			return nil, fmt.Errorf("search %q: %v", name, err)
+		}
+		records = append(records, alb)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, fmt.Errorf("search %q: %v", name, err)
+	}
+	return records, nil
 }
 
 func (s *Service) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	db := s.db
-	fmt.Printf("Req: %s %s\n", r.Host, r.URL.Path) 
-    name := r.URL.Path
-    //write line by line to resp
-    w.Header().Set("Content-Type", "text/html")
-    w.Write("VIDI")
+	//db := s.db
+	fmt.Printf("Req: %s %s\n", r.Host, r.URL.Path)
+	//name := r.URL.Path
+	//write line by line to resp
+	w.Header().Set("Content-Type", "text/html")
+	w.Write([]byte("VIDI"))
 }
 
-func Load(db *sql.DB)(*Service){
+func Load(db *sql.DB) *Service {
 	s := &Service{db: db, err: nil}
 	return s
 }
